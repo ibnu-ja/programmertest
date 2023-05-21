f77de81afe136b27e46acdc9b55c361953571294
Ibnu <ibnu.magang@gmail.com>
2023-05-21 18:54:37 +0700

    fix: revert back amount column to numeric

--- src/main/java/io/ibnuja/programmertest/models/entity/Transaction.java
+++ src/main/java/io/ibnuja/programmertest/models/entity/Transaction.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,6 +31,5 @@ public class Transaction {
 
     private String debitCreditStatus;
 
-    @Column(columnDefinition = "MONEY")
     private BigDecimal amount;
 }
